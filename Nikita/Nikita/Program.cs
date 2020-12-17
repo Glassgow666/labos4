c91cb4bb3ff79cfdeb4d7725423c662e4b9acb40
Glassgow666 <45860787+Glassgow666@users.noreply.github.com>
2020-12-17 12:24:00 +0300

    Add files via upload

---
+++ Nikita/Nikita/Program.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Nikita
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            Graph graph = new Graph();
+            graph.AddVertex("A");
+            graph.AddVertex("B");
+            graph.AddVertex("C");
+            graph.AddVertex("D");
+            graph.AddVertex("E");
+
+            graph.AddEdge("A", "B", 10);
+            graph.AddEdge("B", "C", 15);
+            graph.AddEdge("C", "D", 20);
+            graph.AddEdge("D", "E", 25);
+            graph.AddEdge("E", "A", 30);
+            graph.AddEdge("A", "C", 15);
+            graph.AddEdge("A", "D", 20);
+            graph.AddEdge("B", "D", 20);
+            graph.AddEdge("B", "E", 25);
+            graph.AddEdge("C", "E", 10);
+
+            var dijkstra = new Dijkstra(graph);
+            var path = dijkstra.FindShortestPath("A","E");
+            Console.WriteLine(path);
+            stopWatch.Stop();
+
+            TimeSpan ts = stopWatch.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+    }
+
+    public class GraphEdge
+    {
+        public GraphVertex ConnectedVertex { get; }
+
+        public int EdgeWeight { get; }
+
+        public GraphEdge(GraphVertex connectedVertex, int weight)
+        {
+            ConnectedVertex = connectedVertex;
+            EdgeWeight = weight;
+        }
+    }
+
+    public class GraphVertex
+    {
+        public string Name { get; }
+
+        public List<GraphEdge> Edges { get; }
+
+        public GraphVertex(string vertexName)
+        {
+            Name = vertexName;
+            Edges = new List<GraphEdge>();
+        }
+
+        public void AddEdge(GraphEdge newEdge)
+        {
+            Edges.Add(newEdge);
+        }
+
+        public void AddEdge(GraphVertex vertex, int edgeWeight)
+        {
+            AddEdge(new GraphEdge(vertex, edgeWeight));
+        }
+
+        public override string ToString() => Name;
+    }
+
+    public class Graph
+    {
+        public List<GraphVertex> Vertices { get; }
+
+        public Graph()
+        {
+            Vertices = new List<GraphVertex>();
+        }
+
+        public void AddVertex(string vertexName)
+        {
+            Vertices.Add(new GraphVertex(vertexName));
+        }
+
+        public GraphVertex FindVertex(string vertexName)
+        {
+            foreach(var v in Vertices)
+            {
+                if(v.Name.Equals(vertexName))
+                {
+                    return v;
+                }
+            }
+            return null;
+        }
+
+        public void AddEdge(string firstName, string secondName, int weight)
+        {
+            var v1 = FindVertex(firstName);
+            var v2 = FindVertex(secondName);
+            if(v2!=null && v1!= null)
+            {
+                v1.AddEdge(v2, weight);
+                v2.AddEdge(v1, weight);
+            }
+        }
+
+    }
+
+    public class GraphVertexInfo
+    {
+        public GraphVertex Vertex { get; set; }
+
+        public bool IsUnvisited { get; set; }
+
+        public int EdgesWeightSum { get; set; }
+
+        public GraphVertex PreviousVertex { get; set; }
+
+        public GraphVertexInfo(GraphVertex vertex)
+        {
+            Vertex = vertex;
+            IsUnvisited = true;
+            EdgesWeightSum = int.MaxValue;
+            PreviousVertex = null;
+        }
+    }
+
+    public class Dijkstra
+    {
+        Graph graph;
+        List<GraphVertexInfo> infos;
+
+        public Dijkstra(Graph graph)
+        {
+            this.graph = graph;
+        }
+
+        void InitInfo()
+        {
+            infos = new List<GraphVertexInfo>();
+            foreach(var v in graph.Vertices)
+            {
+                infos.Add(new GraphVertexInfo(v));
+            }
+        }
+
+        GraphVertexInfo GetVertexInfo(GraphVertex v)
+        {
+            foreach(var i in infos)
+            {
+                if(i.Vertex.Equals(v))
+                {
+                    return i;
+                }
+            }
+            return null;
+        }
+
+        public GraphVertexInfo FindUnvisitedVertexWithMinSum()
+        {
+            var minValue = int.MaxValue;
+            GraphVertexInfo minVertexInfo = null;
+            foreach (var i in infos)
+            {
+                if(i.IsUnvisited && i.EdgesWeightSum<minValue)
+                {
+                    minVertexInfo = i;
+                    minValue = i.EdgesWeightSum;
+                }
+            }
+            return minVertexInfo;
+        }
+
+        public string FindShortestPath(string startName, string finishName)
+        {
+            return FindShortestPath(graph.FindVertex(startName), graph.FindVertex(finishName));
+        }
+
+        public string FindShortestPath(GraphVertex startVertex, GraphVertex finishVertex)
+        {
+            InitInfo();
+            var first = GetVertexInfo(startVertex);
+            first.EdgesWeightSum = 0;
+            while (true)
+            {
+                var current = FindUnvisitedVertexWithMinSum();
+                if (current == null)
+                {
+                    break;
+                }
+
+                SetSumToNextVertex(current);
+            }
+
+            return GetPath(startVertex, finishVertex);
+        }
+
+        void SetSumToNextVertex(GraphVertexInfo info)
+        {
+            info.IsUnvisited = false;
+            foreach (var e in info.Vertex.Edges)
+            {
+                var nextInfo = GetVertexInfo(e.ConnectedVertex);
+                var sum = info.EdgesWeightSum + e.EdgeWeight;
+                if (sum < nextInfo.EdgesWeightSum)
+                {
+                    nextInfo.EdgesWeightSum = sum;
+                    nextInfo.PreviousVertex = info.Vertex;
+                }
+            }
+        }
+
+        string GetPath(GraphVertex startVertex, GraphVertex endVertex)
+        {
+            var path = endVertex.ToString();
+            while (startVertex != endVertex)
+            {
+                endVertex = GetVertexInfo(endVertex).PreviousVertex;
+                path = endVertex.ToString() + path;
+            }
+
+            return path;
+        }
+    }
+}
